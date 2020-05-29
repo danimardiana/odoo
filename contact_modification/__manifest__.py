c9156b4cfce2a272df3662f5868e3106a86a9075
jdcprj <sbhavsar@jdcsystems.net>
2020-05-29 15:25:51 +0530

    [ADD] Initial commit for contact_ modification module

---
+++ contact_modification/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Contact Modification",
+    'category': 'Contact Modification',
+    'summary': 'Contact Modification',
+    "author": "",
+    'version': '13.0.0',
+    'description': """
+        """,
+    "depends": [
+        'base','contacts'
+    ],
+    "data": [
+        
+        "views/res_partner_view.xml",
+    ],
+   
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ contact_modification/models/res_partner.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import base64
+import logging
+from odoo import api, fields, models
+from ast import literal_eval
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+
+    company_type = fields.Selection(string='Company Type',
+        selection_add=[('company', 'Customer Company'),('owner', 'Owner'),('management', 'Management')],
+        compute='_compute_company_type', inverse='_write_company_type',store=True)
+    ownership_company_type_id = fields.Many2one('res.partner',string='Owner Ship Company')
+    management_company_type_id = fields.Many2many('res.partner','mgmt_rel','respart_id','mgmt_id',string='Mgmt. Company')
+    is_owner = fields.Boolean(string='Is a Owner', default=False,help="Check if the contact is a Owner")
+    is_management = fields.Boolean(string='Is a Management', default=False,help="Check if the contact is a Management")
+    secondary_user_id = fields.Many2one('res.users', string='Secondary Acct. Manager')
+    national_user_id = fields.Many2one('res.users', string='National Acct. Manager')
+    mangement_company_type = fields.Selection(string='Mgmt. Company Type', selection=[('greystar','Greystar'),('other','Other')])
+    contact_type = fields.Selection(string='Contact type', selection=[('normal','Normal'),('proofing','Proofing Contact'),('reporting','Reporting Contact')],default='normal')
+    contact_company_type_id = fields.Many2one('res.partner',string='Contact Name')
+    company_type_rel = fields.Selection(related='company_type',string='Company Type',)
+    child_id_name = fields.Char("Child Name",compute="child_name_get")
+    
+
+    @api.depends('child_ids','type')
+    def child_name_get(self):
+        for rec in self:
+            if rec.child_ids:
+                rec.child_id_name = ','.join([child.name or child.contact_company_type_id.name or '' for child in rec.child_ids])
+            else:
+                rec.child_id_name = ' '
+
+    @api.depends('is_company','is_owner','is_management')
+    def _compute_company_type(self):
+        for partner in self:
+            if partner.is_company and partner.is_owner == False and partner.is_management == False:
+                partner.company_type = 'company'
+            elif partner.is_owner and partner.is_management == False:
+                partner.company_type = 'owner'
+            elif partner.is_management and partner.is_owner == False:
+                partner.company_type == 'management'
+            else:
+                partner.company_type == 'person'
+
+
+    def properties_owner(self):
+        self.ensure_one()
+        domain = ['|',('ownership_company_type_id', '=',self.id),('ownership_company_type_id','in',self.child_ids.ids)]
+        action = self.env.ref('contact_modification.action_contacts_list').read()[0]
+        context = literal_eval(action['context'])
+        context.update(self.env.context)
+        return dict(action, domain=domain, context=context)
+
+
+    def properties_management(self):
+        self.ensure_one()
+        domain = ['|',('management_company_type_id', 'in',[self.id]),('ownership_company_type_id','in',self.child_ids.ids)]
+        action = self.env.ref('contact_modification.action_contacts_list').read()[0]
+        context = literal_eval(action['context'])
+        context.update(self.env.context)
+        return dict(action, domain=domain, context=context)
+
+    def _write_company_type(self):
+        for partner in self:
+
+            if partner.company_type =='company':
+                partner.is_company = True
+                partner.is_management = False
+                partner.is_owner = False
+            if partner.company_type =='person':
+                partner.is_company = False
+                partner.is_management = False
+                partner.is_owner = False
+
+            if partner.company_type == 'owner':
+                partner.is_owner = True
+                partner.is_management = False
+
+            if partner.company_type =='management':
+                partner.is_management = True
+                partner.is_owner = False
+
+
+    @api.onchange('company_type')
+    def onchange_company_type(self):
+        if self.company_type =='company':
+            self.is_company = True
+            self.is_management = False
+            self.is_owner = False
+
+
+        if self.company_type =='person':
+            self.is_company = False
+            self.is_management = False
+            self.is_owner = False
+
+        if self.company_type == 'owner':
+            self.is_owner = True
+            self.is_management = False
+
+        if self.company_type =='management':
+            self.is_management = True
+            self.is_owner = False
+
+
+    
