9edb22d68195927225d2406e22a6afbba3a69a52
jdcprj <sbhavsar@jdcsystems.net>
2020-07-14 15:22:34 +0530

    [IMP]Display the boolean accordingly

--- clx_retail_pricelist/models/product_category.py
+++ clx_retail_pricelist/models/product_category.py
@@ -16,3 +16,14 @@ class ProductCategory(models.Model):
     retail = fields.Boolean("Retail", default=True)
     wholesale = fields.Boolean("Wholesale")
     management_fee = fields.Boolean("Management Fee")
+
+    @api.onchange('pricing_type')
+    def onchange_pricing_type(self):
+        if self.pricing_type:
+            if self.pricing_type == 'percentage_management_fee':
+                self.retail = True
+                self.wholesale = True
+                self.management_fee = True
+            elif self.pricing_type == 'flat_cost_with_COG':
+                self.retail = True
+                self.wholesale = True
