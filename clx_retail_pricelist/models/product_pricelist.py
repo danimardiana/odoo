7ddb802b6a94d7c2bf3d44e0ba679d4d57e2f312
jdcprj <sbhavsar@jdcsystems.net>
2020-07-16 17:01:56 +0530

    [IMP]set pricelist of mana. company and some new changes in pricelist level

--- clx_retail_pricelist/models/product_pricelist.py
+++ clx_retail_pricelist/models/product_pricelist.py
@@ -7,14 +7,32 @@ from odoo import api, fields, models
 class ProductPricelistItem(models.Model):
     _inherit = "product.pricelist.item"
 
-    is_management_fee = fields.Boolean("Is Management Fee")
     min_price = fields.Float("Min. Price")
     is_fixed = fields.Boolean('Fixed Price')
     is_percentage = fields.Boolean('Percentage')
     is_custom = fields.Boolean('Custom')
+    is_wholesale_percentage = fields.Boolean('Percentage')
+    is_wholesale_formula = fields.Boolean('Formula')
     fixed_mgmt_price = fields.Float('Fixed Price', digits='Product Price')
     percent_mgmt_price = fields.Float('Percentage Price')
+    percent_wholesale_price = fields.Float('Percentage Price')
     min_retail_amount = fields.Float('Minimum Retail Amount')
+    display_management_fee = fields.Boolean(
+        string="Display Management Fee")
+
+    @api.onchange('applied_on', 'product_id', 'product_tmpl_id', 'categ_id')
+    def onchange_product_consistency(self):
+        for item in self:
+            if item.applied_on == '3_global':
+                item.display_management_fee = True
+            elif item.applied_on == '2_product_category' and item.categ_id.management_fee and item.categ_id.wholesale:
+                item.display_management_fee = True
+            elif item.applied_on == '1_product' and item.product_tmpl_id.categ_id.management_fee and item.categ_id.wholesale:
+                item.display_management_fee = True
+            elif item.applied_on == '0_product_variant' and item.product_id.categ_id.management_fee and item.categ_id.wholesale:
+                item.display_management_fee = True
+            else:
+                item.display_management_fee = False
 
     @api.onchange('is_custom')
     def onchange_is_custom(self):
