f24d38b1409885c9bd635f90ed62d48440bfb268
jdcprj <sbhavsar@jdcsystems.net>
2020-07-20 18:18:37 +0530

    [IMP]added new changes in view level

--- clx_retail_pricelist/models/product_pricelist.py
+++ clx_retail_pricelist/models/product_pricelist.py
@@ -17,22 +17,6 @@ class ProductPricelistItem(models.Model):
     percent_mgmt_price = fields.Float('Percentage Price')
     percent_wholesale_price = fields.Float('Percentage Price')
     min_retail_amount = fields.Float('Minimum Retail Amount')
-    display_management_fee = fields.Boolean(
-        string="Display Management Fee")
-
-    @api.onchange('applied_on', 'product_id', 'product_tmpl_id', 'categ_id')
-    def onchange_product_consistency(self):
-        for item in self:
-            if item.applied_on == '3_global':
-                item.display_management_fee = True
-            elif item.applied_on == '2_product_category' and item.categ_id.management_fee and item.categ_id.wholesale:
-                item.display_management_fee = True
-            elif item.applied_on == '1_product' and item.product_tmpl_id.categ_id.management_fee and item.categ_id.wholesale:
-                item.display_management_fee = True
-            elif item.applied_on == '0_product_variant' and item.product_id.categ_id.management_fee and item.categ_id.wholesale:
-                item.display_management_fee = True
-            else:
-                item.display_management_fee = False
 
     @api.onchange('is_custom')
     def onchange_is_custom(self):
