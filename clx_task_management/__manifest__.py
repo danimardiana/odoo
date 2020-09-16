3cd0032ec8867783f2488620367a9468a11c093e
jdcprj <sbhavsar@jdcsystems.net>
2020-09-16 18:37:24 +0530

    - [MOD] clx_task_management : when user delete any task based on configuration system will create dependent task, -  otherwise not created dependent task. in both the case task will be deleted.

--- clx_task_management/__manifest__.py
+++ clx_task_management/__manifest__.py
@@ -3,7 +3,7 @@
 # See LICENSE file for full copyright & licensing details.
 {
     'name': 'CLX Task Management',
-    'version': '13.3.0.0.17',
+    'version': '13.3.0.0.18',
     'summary': 'CLX Task Management',
     'sequence': 1,
     'description': """ CLX Task Management """,
