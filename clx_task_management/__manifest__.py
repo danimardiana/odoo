289b101c187d175cc8b03eee7e0c0703998c45f9
jdcprj <sbhavsar@jdcsystems.net>
2020-10-05 18:29:04 +0530

    [FIX] Fixed issue regardign access rights for new model

--- clx_task_management/__manifest__.py
+++ clx_task_management/__manifest__.py
@@ -3,7 +3,7 @@
 # See LICENSE file for full copyright & licensing details.
 {
     'name': 'CLX Task Management',
-    'version': '13.3.0.0.26',
+    'version': '13.3.0.0.27',
     'summary': 'CLX Task Management',
     'sequence': 1,
     'description': """ CLX Task Management """,
