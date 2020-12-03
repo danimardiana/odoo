5c356f18517f250ddcff48a3c1fb0ffb4f08ed13
jdcprj <sbhavsar@jdcsystems.net>
2020-12-03 12:47:34 +0530

    [REMOVED] removed field submitted_platform

--- contact_modification/__manifest__.py
+++ contact_modification/__manifest__.py
@@ -8,7 +8,7 @@
     'summary': 'To manage Contacts with classification',
     'author': 'CLx Media',
     'website': 'https://conversionlogix.com/',
-    'version': '13.1.1.0.21',
+    'version': '13.1.1.0.22',
     'sequence': 1,
     'license': 'AGPL-3',
     'depends': [
