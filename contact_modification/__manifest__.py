471a0f65aec13f5f0019a6013ada51e477a351d6
jdcprj <sbhavsar@jdcsystems.net>
2020-12-03 14:42:15 +0530

    [ADD] added field

--- contact_modification/__manifest__.py
+++ contact_modification/__manifest__.py
@@ -8,7 +8,7 @@
     'summary': 'To manage Contacts with classification',
     'author': 'CLx Media',
     'website': 'https://conversionlogix.com/',
-    'version': '13.1.1.0.22',
+    'version': '13.1.1.0.23',
     'sequence': 1,
     'license': 'AGPL-3',
     'depends': [
