6c6bb14e10dcaeb133717ff8697e7b254b80b5ce
jdcprj <sbhavsar@jdcsystems.net>
2021-01-16 21:51:24 +0530

    - [FIX] clx_invoice_policy : set description on subscription line when doing upsell and downsell. - [FIX] clx_subscription_creation : added description on subscriptions kanban view

--- clx_invoice_policy/models/sale_order_line.py
+++ clx_invoice_policy/models/sale_order_line.py
@@ -120,6 +120,7 @@ class SaleOrderLine(models.Model):
         inherited method because set interval date on subscription line
         """
         res = super(SaleOrderLine, self)._prepare_subscription_line_data()
+        base_line = self.subscription_id.recurring_invoice_line_ids.filtered(lambda x: x.line_type == 'base')
         if self.order_id and self.order_id.clx_invoice_policy_id and \
                 self.order_id.clx_invoice_policy_id.policy_type == 'advance':
             date_start = self.start_date
@@ -148,5 +149,6 @@ class SaleOrderLine(models.Model):
             res[0][-1].update({
                 'invoice_start_date': self.start_date,
                 'invoice_end_date': date_end,
+                'name': base_line[0].name if base_line else self.product_id.name
             })
         return res
--- clx_subscription_creation/__manifest__.py
+++ clx_subscription_creation/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Subscription Creation',
-    'version': '13.1.1.0.19',
+    'version': '13.1.1.0.20',
     'sequence': 1,
     'description': """ Subscription Creation
      - Subscription is created From sale order line
