3ccdb66f0b909529f08544744cbd0c9b30f5a02f
Vlad-CL <45109788+Vlad-CL@users.noreply.github.com>
2021-04-16 11:59:12 -0700

    Delete grouping_data.py

--- clx_invoice_policy/models/grouping_data.py
+++
@@ -1,8 +0,0 @@
-# descrive special sets of products starting from more complex (more products in set) to simpliest
-products_set_grouping_level = [
-    {'products_list': ['Display', 'Paid Social'],
-     'description': 'Targeted Online Display with Premium Placement',
-     'contract_product_description': '''Targeted online display services aim to identify potential new customers, serve ads that promote your community, and create compelling reasons to visit your website.
-    <br/>*Ads will be served over thousands on locations across the internet - including social media''',
-     }
-]
