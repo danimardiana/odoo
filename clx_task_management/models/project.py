c90139769c2b0c08aa3a0b8c378ef65506c930e7
Todd Davis <toddd@clxmedia.com>
2021-04-22 09:27:48 -0700

    increment prood count

--- clx_task_management/models/project.py
+++ clx_task_management/models/project.py
@@ -364,6 +364,11 @@ class ProjectTask(models.Model):
 
     def write(self, vals):
         res = super(ProjectTask, self).write(vals)
+
+        proof_stage = self.env.ref("clx_task_management.clx_project_stage_6", raise_if_not_found=False)
+        if vals.get("stage_id", False) and self.stage_id.id == proof_stage.id:
+            self.proof_return_count += 1
+
         today = fields.Date.today()
         current_date = today
 
