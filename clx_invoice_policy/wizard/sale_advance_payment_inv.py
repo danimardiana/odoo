06b6849c5648619eb2794c5755a2ab0cc45bbf35
Vlad-CL <45109788+Vlad-CL@users.noreply.github.com>
2021-05-20 14:14:24 -0700

    Invoice Policy correction

--- clx_invoice_policy/wizard/sale_advance_payment_inv.py
+++ clx_invoice_policy/wizard/sale_advance_payment_inv.py
@@ -2,11 +2,11 @@
 # Part of Odoo, CLx Media
 # See LICENSE file for full copyright & licensing details.
 
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from collections import OrderedDict
-from datetime import timedelta
-
+from datetime import timedelta, date
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
@@ -19,12 +19,18 @@ class SaleAdvancePaymentInv(models.TransientModel):
         if not sale_order_id:
             raise UserError(_("You need to sale order for create a invoice!!"))
         sale_order = self.env["sale.order"].browse(sale_order_id)
+        start_date = self.start_date
+        end_date = self.end_date
+        if not start_date:
+            start_date = date.today()
+        if not end_date:
+            end_date = date.today()+relativedelta(months=sale_order.partner_id.clx_invoice_policy_id.num_of_month + 1)
+
         self.env["sale.subscription"].invoicing_invoice_policy_range(
             **{
                 "partner_id": sale_order.partner_id.id,
+                "partner":sale_order.partner_id,
                 "order_id": sale_order_id,
-                "start_date": self.start_date,
-                "end_date": self.end_date,
             }
         )
         if self._context.get("open_invoices", False):
