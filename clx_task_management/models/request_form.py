f153623d6d3932a2f5a88920d9dd1f6188760c19
Todd Davis <toddd@clxmedia.com>
2021-10-05 18:03:52 -0700

    remove unused import

--- clx_task_management/models/request_form.py
+++ clx_task_management/models/request_form.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo, CLx Media
 # See LICENSE file for full copyright & licensing details.
-from requests.api import request
+# from requests.api import request
 from odoo import fields, api, models, _
 from odoo.exceptions import UserError, ValidationError, Warning
 from dateutil.relativedelta import relativedelta
