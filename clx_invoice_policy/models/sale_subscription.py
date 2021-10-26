15c51ecb83a429ed53656309ad20aa1530ded539
Vlad-CL <45109788+Vlad-CL@users.noreply.github.com>
2021-10-26 15:28:26 -0700

    Fix for data export

--- clx_invoice_policy/models/sale_subscription.py
+++ clx_invoice_policy/models/sale_subscription.py
@@ -507,9 +507,10 @@ class SaleSubscription(models.Model):
             return []
         start_date = kwargs["start_date"]
         partner_id = kwargs.get("partner_id", False)
-        subscripion_line = kwargs.get("subscripion_line", False)
+        subscripion_line = kwargs.get("subscription_line", False)
         grouping_levels = kwargs.get("grouping_levels", grouping_data.ALL_FLAGS_GROUPING)
         contract_mode = kwargs.get("contract_mode", False)
+
         def initial_order_data(line, partner_id):
             price, price_full, coop_coef = line.period_price_calc(start_date, partner_id, contract_mode)
             product_variant = ""
